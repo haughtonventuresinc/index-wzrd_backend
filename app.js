d9c5d1bbc4dd60c11a75ae281de080235ac55aec
ARTSKY <info@artsky.io>
2024-05-23 21:58:42 +0500

    coes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const pricingRoute = require("./pricingRoute");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 mongoose
