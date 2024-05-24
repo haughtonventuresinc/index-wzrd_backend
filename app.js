d7847947210c9c57d874ab38ec46ba88cb7ed6df
ARTSKY <info@artsky.io>
2024-05-25 04:56:27 +0500

    added cors 2

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
@@ -7,7 +7,7 @@ const pricingRoute = require("./pricingRoute");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 mongoose
