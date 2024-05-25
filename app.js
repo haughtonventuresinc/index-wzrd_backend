fbc3e24d4cd3a3af1ad3e9fb5702c687c2e0e216
ARTSKY <info@artsky.io>
2024-05-25 20:01:05 +0500

    cors

--- app.js
+++ app.js
@@ -7,13 +7,21 @@ const pricingRoute = require("./pricingRoute");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
+// Configure CORS
+const corsOptions = {
+  origin: "*", // Adjust this to specify the allowed origin(s)
+  methods: "GET,POST,PUT,DELETE,OPTIONS",
+  allowedHeaders:
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log("ERROR:DB not connected"));
+  .catch((err) => console.log("ERROR: DB not connected"));
 
 app.get("/", (req, res) => {
   res.json("Hello Server");
@@ -22,5 +30,5 @@ app.get("/", (req, res) => {
 app.use("/api/v1/pricing", pricingRoute);
 
 app.listen(port, () => {
-  console.log(`server is listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
